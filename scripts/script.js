2dab7e78c2e449e7a246679cd04f7d9b913604af
Anna Agronomovs <a.agronomovs@yandex.ru>
2021-08-01 21:57:49 +0300

    Feat: create delete icon and render cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,31 @@
+// Новые карточки
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
 // Делаем выборку DOM элементов
 const popupOpenButtonElement = document.querySelector('.button_edit');
 const popupElement = document.querySelector('.popup');
@@ -7,6 +35,20 @@ let nameInput = formElement.querySelector('.popup__input_field_name');
 let jobInput = formElement.querySelector('.popup__input_field_job');
 let title = document.querySelector('.profile__title');
 let info = document.querySelector('.profile__text');
+const cardsContainer = document.querySelector('.cards');
+
+
+// Добавление карточек из массива
+initialCards.forEach(function (element) {
+    const cardTemplate = document.querySelector('#template').content;
+    const cardElement = cardTemplate.querySelector('.item').cloneNode(true);
+    cardElement.querySelector('.item__title').textContent = element.name;
+    cardElement.querySelector('.item__photo').src = element.link;
+
+    cardsContainer.append(cardElement);
+})
+
+
 
 /**
  * Функция для переключения попапа
